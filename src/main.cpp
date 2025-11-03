faf58e29bdf5f59768584e65e736beacd95215a1
Tobias Binkowski <sessl3r@gamil.com>
2025-11-03 11:04:14 +0100

    arduino: move printing into a function

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,16 @@ EnergyMonitor emonL1;
 EnergyMonitor emonL2;
 EnergyMonitor emonL3;
 
+void print_phases(const char* opt, double l1, double l2, double l3) {
+	Serial.print(opt);
+	Serial.print(",");
+	Serial.print(l1);
+	Serial.print(",");
+	Serial.print(l2);
+	Serial.print(",");
+	Serial.println(l3);
+}
+
 void setup() {
 	Serial.begin(115200);
 	emonL1.current(0, 26.4);
@@ -21,13 +31,10 @@ void loop() {
 		I[0] += emonL1.calcIrms(SAMPLES);
 		I[1] += emonL2.calcIrms(SAMPLES);
 		I[2] += emonL3.calcIrms(SAMPLES);
+		print_phases("DBG", I[0], I[1], I[2]);
 	}
 	I[0] /= LOOPS;
 	I[1] /= LOOPS;
 	I[2] /= LOOPS;
-	Serial.print(I[0]*234.0);
-	Serial.print(",");
-	Serial.print(I[1]*234.0);
-	Serial.print(",");
-	Serial.println(I[2]*234.0);
+	print_phases("NOM", I[0]*234.0, I[1]*234.0, I[2]*234.0);
 }
