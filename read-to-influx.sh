535aec91acbf4ba001bf8a5f384164c9fa079394
Tobias Binkowski <sessl3r@gamil.com>
2025-11-03 11:04:30 +0100

    cleanup read script

--- read-to-influx.sh
+++ read-to-influx.sh
@@ -13,13 +13,12 @@ stty -F $DEVICE 115200 raw -clocal -echo icrnl min 1
 while read TEMP; do
 	[ -z $TEMP ] && continue
 	P1=$(echo $TEMP | cut -d',' -f1)
-	P1=$(echo "$P1-20" | bc)
+	P1=$(echo "val=$P1-20; if ( val < 0 ) 0 else val" | bc)
 	P2=$(echo $TEMP | cut -d',' -f2)
-	P2=$(echo "$P2-20" | bc)
+	P2=$(echo "val=$P2-20; if ( val < 0 ) 0 else val" | bc)
 	P3=$(echo $TEMP | cut -d',' -f3 | tr -d $'\r')
-	P3=$(echo "$P3-20" | bc)
+	P3=$(echo "val=$P3-20; if ( val < 0 ) 0 else val" | bc)
 	curl -XPOST "http://localhost:8086/write?db=openhab" \
 		--header "Authorization: Token openhab:openhab" \
 		--data-binary "$MEASURE l1=${P1},l2=${P2},l3=${P3}" &>/dev/null
 done < $DEVICE
-
